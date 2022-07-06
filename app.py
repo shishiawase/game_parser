51c2b60b4c6098afe30544147337a9cfa0067d9c
shishiawase <53081951+shishiawase@users.noreply.github.com>
2022-07-06 21:11:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import discord
 import asyncio
 import httpx
 import json
+from discord.ext import tasks
 from config import settings
 
 client = discord.Client()
@@ -44,51 +45,50 @@ def parseRSS(url):
 
     return game
 
+@tasks.loop(seconds=15)
 async def check(last):
     await client.wait_until_ready()
 
-    while True:
+    res = parseRSS('https://tuttop.com/rss.xml')
+    text = { 'desc': res['desc'], 'sys': '', 'about': ''}
 
-        res = parseRSS('https://tuttop.com/rss.xml')
-        text = { 'desc': res['desc'], 'sys': '', 'about': ''}
+    if last != res['title'].lower():
+        last = res['title'].lower()
+        with open('./last.txt', 'w') as f:
+            f.write(last)
 
-        if last != res['title'].lower():
-            last = res['title'].lower()
-            with open('./last.txt', 'w') as f:
-                f.write(last)
+        resp = httpx.get(res['link']).text
+        start = resp.find('https://tuttop.com/torrent')
+        end = resp.find('.torrent', start)
+        url = resp[start:end + 8]
 
-            resp = httpx.get(res['link']).text
-            start = resp.find('https://tuttop.com/torrent')
-            end = resp.find('.torrent', start)
-            url = resp[start:end + 8]
-
-            db = rJson()
-            mention = []
+        db = rJson()
+        mention = []
         
-            for u in db.keys():
-                for t in db[u]:
-                    if last.find(t) != -1:
-                        mention.append(f'<@{u}>')
+        for u in db.keys():
+            for t in db[u]:
+                if last.find(t) != -1:
+                    mention.append(f'<@{u}>')
 
-            for i in res['sys']:
-                word = i.partition(':')[0]
-                i = i.replace(word, f"`{word}`")
-                text['sys'] += (i + '\n')
-            for i in res['about']:
-                word = i.partition(':')[0]
-                i = i.replace(word, f"`{word}`")
-                text['about'] += (i + '\n')
+        for i in res['sys']:
+            word = i.partition(':')[0]
+            i = i.replace(word, f"`{word}`")
+            text['sys'] += (i + '\n')
+        for i in res['about']:
+            word = i.partition(':')[0]
+            i = i.replace(word, f"`{word}`")
+            text['about'] += (i + '\n')
         
-            embed = discord.Embed(
-                color=0x3498db,
-                title=res['title'],
-                description=f"{text['desc']}\n\n\n**Системные требования:**\n{text['sys']}\n**Об игре:**\n{text['about']}\n[Подробности]({res['link']})\n[Cкачать торрент]({url})"
-                )
-            embed.set_image(url=res['img'])
+        embed = discord.Embed(
+            color=0x3498db,
+            title=res['title'],
+            description=f"{text['desc']}\n\n\n**Системные требования:**\n{text['sys']}\n**Об игре:**\n{text['about']}\n[Подробности]({res['link']})\n[Cкачать торрент]({url})"
+            )
+        embed.set_image(url=res['img'])
 
-            channel = client.get_channel(settings['channel'])
-            await channel.send(' '.join(mention), embed=embed)
-            await asyncio.sleep(15)
+        channel = client.get_channel(settings['channel'])
+        await channel.send(' '.join(mention), embed=embed)
+        #await asyncio.sleep(15)
 
 @client.event
 async def on_ready():
@@ -99,12 +99,20 @@ async def on_message(msg):
     if msg.content.startswith('***'):
         db = rJson()
         title = msg.content.replace('*** ', '')
-        if db.get(msg.author.id): db[msg.author.id].append(title.lower())
-        else: db[msg.author.id] = [title.lower()]
-        wJson(db)
-
         channel = msg.channel
+
+        if str(msg.author.id) in db.keys():
+            print('yes')
+            if title.lower() not in db[str(msg.author.id)]:
+                db[str(msg.author.id)].append(title.lower())
+            else:
+                await channel.send(f'`{title} уже существует в вашей коллекции.`')
+                return
+        else: db[str(msg.author.id)] = [title.lower()]
+        wJson(db)
         await channel.send(f'Упоминание о `{title}`, добавлено в коллекцию пользователя - `{msg.author.nick}`.')
 
-client.loop.create_task(check(last))
+#client.loop.create_task(check(last))
+db = rJson()
+print(db.keys())
 client.run(settings['token'])
