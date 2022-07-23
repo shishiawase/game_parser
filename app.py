28d00cb4963792914d73340ece2f922d40d80019
shishiawase <53081951+shishiawase@users.noreply.github.com>
2022-07-23 07:55:06 +0500

    decorate

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ async def on_message(msg):
                 embed = discord.Embed(
                     color=randint(0, 0xFFFFFF),
                     title=game['title'],
-                    description=f"\n[Подробности]({game['link']})\n[Cкачать торрент]({game['torrent']}) {game['size']}"
+                    description=f"\n`Размер`: {game['size']}\n[Подробности]({game['link']})\n[Cкачать торрент]({game['torrent']})"
                 )
                 embed.set_image(url=game['img'])
                 await channel.send(embed=embed)
@@ -193,7 +193,7 @@ async def on_message(msg):
                     embed = discord.Embed(
                         color=randint(0, 0xFFFFFF),
                         title=game['title'],
-                        description=f"\n[Подробности]({game['link']})\n[Cкачать торрент]({game['torrent']}) {game['size']}"
+                        description=f"\n`Размер`: {game['size']}\n[Подробности]({game['link']})\n[Cкачать торрент]({game['torrent']})"
                     )
                     embed.set_image(url=game['img'])
                     await channel.send(embed=embed)
